d3bacdd39c1b604e48fa50421fc72f98169de6a2
Patrick <patasax22@yahoo.com>
2021-10-28 16:28:19 -0400

    switch case added

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const sequelize = require('./config/connection');
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Good@eats1234',
-    database: 'employee_tracker',
-    port: 3306
-});
+const PORT = process.env.PORT || 3001;
+
+const choice = () => {
+    inquirer.prompt({
+        type: "list",
+        name: "first",
+        message: "What would you like to do?",
+        choices: [
+            "View all Departments",
+            "View all Roles",
+            "View all Employees",
+            "Add Department",
+            "Add Role",
+            "Add Employee",
+            "Update Employee Role",
+            "Exit"
+        ],
+    })
+    .then(({ choice }) => {
+        console.log(choice);
+        switch (choice) {
+            case "View all Departments":
+                viewDepartments();
+                break;
+            case "View all Roles":
+                viewRoles();
+                break;
+            case "View all Employees":
+                viewEmployees();
+                break;
+            case "Add Department":
+                addDepartment();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "Add Employee":
+                addEmployee();
+                break;
+            case "Update Employee Role":
+                updateEmployeeRole();
+                break;
+            default:
+                connection.end()
+        }
+    });
+};
 
 
 
-connection.connect((err) => {
-    if (err) throw err
-    prompt();
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
 });
