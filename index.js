4a30e8b73d6837559c4b0c79bed0dbe912419d1f
Patrick <patasax22@yahoo.com>
2021-10-28 16:34:32 -0400

    view methods added

--- index.js
+++ index.js
@@ -51,7 +51,29 @@ const choice = () => {
     });
 };
 
+const viewDepartments = () => {
+    connection.query("SELECT name FROM department", (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        choice();
+    });
+};
 
+const viewRoles = () => {
+    connection.query("SELECT name AS department_name, title, salary FROM role INNER JOIN department ON department.id = role.department_id", (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        choice();
+    });
+};
+
+const viewEmployees = () => {
+    connection.query("SELECT first_name, last_name, title, salary FROM employee INNER JOIN role ON role.id = employee.role_id", (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        choice();
+    });
+};
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
